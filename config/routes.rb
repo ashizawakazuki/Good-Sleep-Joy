89eff3a7ed66ed635ba79b5e68cf6e5f302f5659
ashizawakazuki <ashizawa.kazuki@gmail.com>
2024-12-07 08:55:26 +0900

    add:アイテム投稿画面への導線を実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get 'item_posts', to: 'item_posts#index' #あとでresoucesを使って書き直す
-  get 'item_posts/new', to: 'item_posts#new'
+
+  #resourcesは「RESTfulなルーティングを簡単にまとめて設定できる便利な仕組み」
+  #RESTfulは「Webアプリケーションの設計において、リソース（データ）を操作するための統一されたルールや考え方」
+  #get 'item_posts', to: 'item_posts#index'　→ item_posts_path(resourcesと同じ)
+  #get 'item_posts/new', to: 'item_posts#new' → item_posts_new_path(resourcesと異なるpath)
+  resources :item_posts, only: %i[new index]
   # Defines the root path route ("/")
   # root "posts#index"
 end
