4a0c3d5aa8072e59a4f580103040d0b59afd0dcd
ashizawakazuki <ashizawa.kazuki@gmail.com>
2024-12-06 23:45:46 +0900

    add:アイテム投稿画面の作成

--- app/controllers/item_posts_controller.rb
+++ app/controllers/item_posts_controller.rb
@@ -2,4 +2,9 @@ class ItemPostsController < ApplicationController
   def index
     @item_posts = ItemPost.includes(:user)
   end
+  #上記は全ての（複数の）データを格納するから複数形の@item_posts、下記は１つの投稿を格納するから単数系の@item_post
+  def new
+    @item_post = ItemPost.new # ユーザーが入力したデータを受け取れるよう空のインスタンスを用意し、ビューに渡す
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get 'item_posts', to: 'item_posts#index' #あとでresoucesを使って書き直す
+  get 'item_posts/new', to: 'item_posts#new'
   # Defines the root path route ("/")
   # root "posts#index"
 end
