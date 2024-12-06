1db2aabf48a7f264da38212828a58567fd4d9b73
ashizawakazuki <ashizawa.kazuki@gmail.com>
2024-12-06 14:27:29 +0900

    add:item_postsテーブルを作成

---
+++ app/models/item_post.rb
@@ -0,0 +1,7 @@
+class ItemPost < ApplicationRecord
+  #バリデーション設定(DBに保存される前にデータが正しい形式や条件を満たしているかを「presence」や「length」で確認)
+  validates :title, presence: true, length: { maximum: 100 } #空でない、最大100文字
+  validates :body, presence: true, length: { maximum:  1000 } #空でない、最大1000文字
+
+  belongs_to :user
+end
