20e0efff21c88b7fcefda7e2238a248e3a526675
ashizawakazuki <ashizawa.kazuki@gmail.com>
2024-12-10 02:51:07 +0900

    add:アイテム投稿詳細画面の機能追加

--- app/controllers/item_posts_controller.rb
+++ app/controllers/item_posts_controller.rb
@@ -13,11 +13,13 @@ class ItemPostsController < ApplicationController
     @item_post = ItemPost.new # ユーザーが入力したデータを受け取れるよう空のインスタンスを用意し、ビューに渡す
   end
   #③送られてきたものに対応するコントローラのcreateアクションの中で、
-  #・「current_user」は今ログインしてる人のIDを設定
-  #・「boards」でログインしてるユーザーの投稿を紐付ける準備（投稿一覧を取得）
-  #・「build」でインスタンス（新しいレコードを作成する準備）を生成（空ではない）
-  #・「（board_params）」でフォームで送られてきたデータを絞り込み、代入される
-  #④それを@boardに入れる
+  #・「current_user」：今ログインしているユーザーのIDを取得
+  #・「item_posts」：ユーザーと関連づけられた投稿を準備（投稿一覧を取得）（アソシエーションによるもの
+    #→current_userのitem_postsテーブルを参照（持ってくる）イメージ
+  #・buildメソッドでparamsで持ってきたデータを入れて新しいインスタンスを作成
+    #→イメージは、「データベースに保存される前のレコードの枠組み」を作るって感じ(ノーション参照)
+  #・「（board_post_params）」でフォームで送られてきたデータを絞り込み、代入される（board_post_paramsはストロングパラメータ）
+  #④それを@item_postに入れる
   def create #createアクションには新しい投稿をDBに保存する処理を記述する
     @item_post = current_user.item_posts.build(item_post_params)
     if @item_post.save
@@ -27,14 +29,33 @@ class ItemPostsController < ApplicationController
     end
   end
 
-  def show #あとで確認
+  #例えばhttp〜item_posts/30のようなリクエストが来たら、以下のようにparamsに格納されてRailsの世界にくる
+  #params = { "id" => "30" }
+  #Railsの世界に来たparamsを、以下のshowコントローラでparams[:id]と書いて、データベースからレコードを持ってくる
+  #params[:id] = 30　と考えてよい
+  #@item_postにIDが30の投稿が格納されてshowファイルへ
+  def show 
     @item_post = ItemPost.find(params[:id])
   end
   
-  def edit #あとで確認
+  #今ログインしているユーザーの投稿（DBのテーブルの表を思い浮かべる。）のうち、paramsに格納されているIDの該当するものをfindで探して、インスタンス変数に代入している
+  #インスタンス変数は、form_withがあるshow.html.erbへ。
+  def edit 
     @item_post = current_user.item_posts.find(params[:id])
   end
-     
+
+  #①「@item_post = current_user.item_posts.find(params[:id])」で、更新対象のデータをまず持ってくる
+  #②createアクション同様、フォームで更新した内容がparamsで送られてくる（ストロングパラメータであるitem_post_paramsで絞り込み）
+  #③updateメソッドで、フォームからきた新しいデータに、用意した更新対象の@item_postを更新する
+  #④成功時は詳細画面へ、失敗時はもう一度編集画面を描写する
+  def update
+    @item_post = current_user.item_posts.find(params[:id])
+    if @item_post.update(item_post_params)
+      redirect_to item_post_path, notice: '投稿が成功しました。'
+    else
+      render :edit, status: :unprocessable_entity #あとでフラッシュメッセージ実装する
+    end
+  end
 
   private
 
