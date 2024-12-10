18be3cd47d20f7c8078dc8ba702823c9895e2c5f
ashizawakazuki <ashizawa.kazuki@gmail.com>
2024-12-11 07:10:35 +0900

    コメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,14 @@ class User < ApplicationRecord
 
   has_many :item_posts, dependent: :destroy #ユーザーが削除された時、関連するモデル（item_post）のレコード削除される
 
+
+ #これは独自のメソッドを定義。Userモデルに書いているので、Userインスタンス（userテーブルから取り出したデータが入っているインスタンス）に対して使えるメソッド
+ #resourceは引数であり、ビューファイルの「current_user_own?(item_post)」でitem_postがresourceに代入される
+ #結果、own?メソッドの中で「item_post.user.id == id」という文になる
+ #idはRailsが用意しているもので、現在ログインしているユーザー（current_user）を指す（本当はself.idらしいが、現時点ではよくわかってない）
+
   def own?(resource)
-    resource.user_id == id
+    resource.user_id == id #左辺「この投稿のID」と、右辺「現在ログインしてるID」は一致してるのかを見ている
   end
 
 end
