dd233db52d9cad681336d8b93f6ca9a1c1d346d8
ashizawakazuki <ashizawa.kazuki@gmail.com>
2024-12-24 12:13:10 +0900

    fix:コメントアウトを追加

--- app/uploaders/item_post_image_uploader.rb
+++ app/uploaders/item_post_image_uploader.rb
@@ -8,6 +8,8 @@ class ItemPostImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+  # store_dir は、アップロードされたファイルの保存先（ディレクトリ）を指定するメソッド
+  # ここで public/uploaders/uploads/item_post/item_imageにIDごとに画像を保存する設定をしている
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -33,12 +35,15 @@ class ItemPostImageUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
+  # キャリーウェーブがアップロードできるファイル形式（拡張子）を制限している
+  # extension_allowlist メソッドは、**CarrierWaveが提供するメソッド
   def extension_allowlist
     %w(jpg jpeg gif png)
   end
 
-  #S3を使用するのに記述した箇所
-  #Rails.env.production? の場合(つまり本番環境)で storage :fog を使用し、それ以外の環境（開発・テスト）では storage :file する
+  # S3を使用するのに記述した箇所
+  # Railsの環境によってファイルの保存方法を切り替えるための設定
+  # Rails.env.production? で、trueの場合(つまり本番環境)で storage :fog を使用し、それ以外の環境（開発・テスト）では storage :file する
   if Rails.env.production?
     storage :fog
   else
