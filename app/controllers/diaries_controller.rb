1f5e7f4b2386c7eec4b97e13aa7185add559836d
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-01-28 16:44:00 +0900

    add:投稿した日記をマイページに表示されるよう実装”

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -10,12 +10,12 @@ class DiariesController < ApplicationController
   def create
     @diary = current_user.diaries.build(diary_params)
     if @diary.save
-      redirect_to diaries_path, notice: '作成しました！今日も一日お疲れ様でした！'
+      redirect_to profile_path, notice: '作成しました！今日も一日お疲れ様でした！'
     else
       flash.now[:alert] = '作成に失敗しました。入力内容を確認してください'
       render :new, status: :unprocessable_entity
-    end
   end
+end
 
 private
 
