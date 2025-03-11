a0b2ee4275aac841de9485aaa7abee9618137a03
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-03-11 20:17:23 +0900

    アイテム投稿時のデフォルト画像を修正

--- app/uploaders/item_post_image_uploader.rb
+++ app/uploaders/item_post_image_uploader.rb
@@ -21,7 +21,10 @@ class ItemPostImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
- 
+  def default_url
+    'no_image.png'
+  end
+
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
