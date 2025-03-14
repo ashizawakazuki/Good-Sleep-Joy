a272bc0026abf7012589496affcfc5ff69777648
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-03-15 07:14:53 +0900

    fix:新規登録時（ログイン時）の遷移先をトップページに指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # devise導入により使えるようになったメソッドで、新規登録（ログイン時）の遷移先を指定できるもの
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   protected
 
   def configure_permitted_parameters
