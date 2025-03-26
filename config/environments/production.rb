61fa216432301f8fc6fcf813e8b5853cc6c9dc02
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-03-26 11:37:03 +0900

    add:config/environments/production.rbに設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Rails.application.configure do
+    config.hosts << 'good-sleep-joy.com'
+  end
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
