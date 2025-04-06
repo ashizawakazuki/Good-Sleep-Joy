da5efd601ba87e9fecaaa00f567e537cf0d07f68
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-04-06 21:59:27 +0900

    fix:コメントアウト

--- app/javascript/hamburger.js
+++ app/javascript/hamburger.js
@@ -1,17 +1,31 @@
+// turbo:loadにしておくと、turboでのページ遷移時でも実行されるようになる
+// turboはページ全体をリロードせず、必要な部分だけ書き換えて、画面遷移を早くする仕組み（例えばヘッダーは残してbodyの部分だけ切り替えたり）
 document.addEventListener('turbo:load', () => {
-const btn =  document.getElementById('hamburger');
-const xmark =  document.getElementById('xmark');
-const content =  document.getElementById('humburger-content');
+  const btn =  document.getElementById('hamburger');
+  const xmark =  document.getElementById('xmark');
+  const content =  document.getElementById('hamburger-content');
+  
+  // if文で定数（要素）があるときのみイベントを発生させる
+  if (btn && xmark && content) {
+    // クリック時にイベントを発生させる
+    btn.addEventListener('click', () => {
+      // btnに入れた要素のクラスにhiddenというクラスを追加して隠している
+      btn.classList.add('hidden');
+      // xmarkに入れた要素のクラスのhiddenというクラスを削除している
+      xmark.classList.remove('hidden');
+      // contentに入れた要素のクラスのhiddenというクラスを切り替えている
+      // hiddenがついてなければ追加（非表示）、hiddenがついてたら削除（表示）
+      content.classList.toggle('hidden');
+    });
 
-btn.addEventListener('click', () => {
-    btn.classList.add('hidden');
-    xmark.classList.remove('hidden');
-    content.classList.toggle('hidden');
-});
-
-xmark.addEventListener('click', () => {
-    xmark.classList.add('hidden');
-    btn.classList.remove('hidden');
-    content.classList.toggle('hidden');
-});
+    xmark.addEventListener('click', () => {
+      // xmarkに入れた要素のクラスにhiddenというクラスを追加して隠している
+      xmark.classList.add('hidden');
+      // btnに入れた要素のクラスのhiddenというクラスを削除している
+      btn.classList.remove('hidden');
+      // contentに入れた要素のクラスのhiddenというクラスを切り替えている
+      // hiddenがついてなければ追加（非表示）、hiddenがついてたら削除（表示）
+      content.classList.toggle('hidden');
+    });
+  };
 });
