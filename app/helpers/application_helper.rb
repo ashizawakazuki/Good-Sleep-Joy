3f5afd2274a8002847a3ab4a3b4d86ddf1dd951b
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-04-11 10:15:54 +0900

    fix;軽微な修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,22 +14,18 @@ module ApplicationHelper
       また、1日の最後にその日楽しかった出来事を3つ書き出すことで、毎日ポジティブな気持ちで眠りにつくことができます。',
       canonical: "https://good-sleep-joy.com/",
       separator: '|',
-      # icon: [
-      #   { href: image_url('logo.png') },
-      #   { href: image_url('top_image.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
-      # ],
       og: {
         site_name: :site,
         title: :title,
         description: :description,
         type: 'website',
         url: "https://good-sleep-joy.com/",
-        image: asset_url('ogp.png'),# 配置するパスやファイル名によって変更する
+        image:'https://good-sleep-joy.com/ogp.png',# 配置するパスやファイル名によって変更する
         local: 'ja-JP',
       },
       twitter: {
-        card: 'summary_large_image', # Twitterで表示する場合は大きいカードに変更
-        image: asset_url('ogp.png'),# 配置するパスやファイル名によって変更
+        card: 'summary_large_image',
+        image: 'https://good-sleep-joy.com/ogp.png',# 配置するパスやファイル名によって変更
       }
     }
   end
