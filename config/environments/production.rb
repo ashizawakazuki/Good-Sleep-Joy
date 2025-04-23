ade46bf9591f28e301c7378d71d5dbceadd7114e
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-04-23 14:56:21 +0900

    fix:ドメインを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'good-sleep-joy.com/',
+    domain:               'good-sleep-joy.com',
     user_name:            ENV['MAILER_SENDER'],
     password:             ENV['MAILER_PASSWORD'],
     authentication:       'plain',
