4e3c0999f07cee156957c2869a11c65c877bc81e
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-05-08 15:53:03 +0900

    user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,31 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # deviseによるログイン等の機能をUserモデルに組み込んでいる
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
 
-  has_many :item_posts, dependent: :destroy #ユーザーが削除された時、関連するモデル（item_post）のレコード削除される
+  has_many :item_posts, dependent: :destroy
   has_many :habit_posts, dependent: :destroy
   has_many :diaries, dependent: :destroy
   has_many :item_likes, dependent: :destroy
-  # 下記では単なる関連付け（元々のアソシエーション設定）ではなく、liked_item_postsという新たなメソッドを作るために定義している
+
+  # 下記では単なる関連付け（元々のアソシエーション設定）ではなく、liked_item_postsという新たなメソッドを作るために定義
   # through: :item_likes で中間テーブルを経由し、item_postのデータを持ってくる
-  # つまりこの一行で「ユーザーがいいねした投稿」の一覧を簡単に取得できるメソッド を作っている。
+  # この一行で「ユーザーがいいねした投稿」の一覧を簡単に取得できるメソッドを作成
   has_many :liked_item_posts, through: :item_likes, source: :item_post
   has_many :habit_likes, dependent: :destroy
   has_many :liked_habit_posts, through: :habit_likes, source: :habit_post
   has_many :item_comments, dependent: :destroy
   has_many :habit_comments, dependent: :destroy
 
- #これは独自のメソッドを定義。Userモデルに書いているので、Userインスタンス（userテーブルから取り出したデータが入っているインスタンス）に対して使えるメソッド
- #resourceは引数であり、ビューファイルの「current_user_own?(item_post)」でitem_postがresourceに代入される
- #結果、own?メソッドの中で「item_post.user.id == id」という文になる
- #idはRailsが用意しているもので、現在ログインしているユーザー（current_user）を指す（本当はself.idらしいが、現時点ではよくわかってない）
- #アプリのどこでも使えるが、実際にはコントローラとビューで使う（ルーティングとかでは使われない）
+  # 独自のメソッドを定義。Userモデルに書いているので、Userインスタンスに対して使えるメソッド
+  # ビューファイルの「current_user_own?(item_post)」でitem_postがresourceに代入される
   def own?(resource)
-    resource&.user_id == id #左辺「この投稿のID」と、右辺「現在ログインしてるID」は一致してるのかを見ている
+    resource&.user_id == id #左辺「この投稿のID」と、右辺「現在ログインしてるID」は一致してるのかを確認
   end
 
-   #上記にも書いたように「liked_item_posts」は「ユーザーがいいねした投稿」の一覧を簡単に取得できるメソッド
+  # 「liked_item_posts」は「ユーザーがいいねした投稿」の一覧を取得できるメソッド
   def item_like(item_post)
     liked_item_posts << item_post
   end
@@ -61,7 +57,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
 
       user.name = auth.info.name
-      user.avatar = auth.info.image  # ←avatarにはimageを使う！
+      user.avatar = auth.info.image
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
