a3e01b069ebf63b9fe8397574e5974dbb4294cdd
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-05-10 05:18:25 +0900

    fix:habit_postsテーブルのカラムを修正(null falseを追加)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_09_194636) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_09_200058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,11 +46,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_09_194636) do
   create_table "habit_posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "habit_post_image"
-    t.bigint "habit_tag_id"
+    t.string "habit_post_image", null: false
+    t.bigint "habit_tag_id", null: false
     t.index ["habit_tag_id"], name: "index_habit_posts_on_habit_tag_id"
     t.index ["user_id"], name: "index_habit_posts_on_user_id"
   end
