7bae4453ca09ce1d4f7eb855f6621a6cad6867f5
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-05-31 15:53:10 +0900

    fix:日記投稿のタイトルのバリデーションを変更

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,5 +1,5 @@
 class Diary < ApplicationRecord
   validates :date, presence: true
-  validates :title, presence: true, length: { maximum: 20 }
+  validates :title, presence: true, length: { maximum: 50 }
   belongs_to :user
 end
