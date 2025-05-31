d86dc58c785ac4d91dca36f4b802fe525531a3c3
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-05-31 15:52:09 +0900

    fix:アイテム投稿のタイトルのバリデーションを変更

--- app/models/item_post.rb
+++ app/models/item_post.rb
@@ -1,6 +1,6 @@
 class ItemPost < ApplicationRecord
   #バリデーション設定(DBに保存される前にデータが正しい形式や条件を満たしているかを「presence」や「length」で確認)
-  validates :title, presence: true, length: { maximum: 100 } #空でない、最大100文字
+  validates :title, presence: true, length: { maximum: 50 } #空でない、最大50文字
   validates :body, presence: true, length: { maximum: 1000 } #空でない、最大1000文字
 
   belongs_to :user
