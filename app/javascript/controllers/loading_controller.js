e51f07204cc4101576a30f9c1a223e847244f296
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-06-01 10:36:19 +0900

    feat:loading_controller.jsを記述

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -2,6 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="loading"
 export default class extends Controller {
+  static targets = ["spinner"]
+  
   connect() {
+    this.hide()
+  }
+  
+  show() {
+    this.spinnerTarget.classList.remove("hidden")
+  }
+  
+  hide() {
+    this.spinnerTarget.classList.add("hidden")
   }
 }
