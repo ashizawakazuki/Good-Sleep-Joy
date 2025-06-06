100dc20113b4588ffd1370a550223ed923145498
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-06-06 20:27:09 +0900

    add:ローディングアニメーションに関するコメントアウトを記述

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -1,21 +1,28 @@
+// stimulusが用意している「Controller」クラスをここで使うための宣言（必ず書くもの）
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="loading"
+// ここからstimulusのコントローラの内容を記述するという意味（ここからstimulusコントローラの内容が始まる）
 export default class extends Controller {
-  static targets = ["spinner"]
+  // HTMLの「data-loading-target = "spiner"」となっている要素を取得し、このコントローラの中では「this.spinnerTarget」という形で操作できるよう設定
+  static targets = ["spinner"] // →「this.spinnerTarget」として使える
   
+  // connectはstimulusによるメソッドで「data-controller」で囲った要素が読み込まれた時に自動で実行される
   connect() {
     this.hide()
-    // ブラウザバックでローディングアニメーションが表示されないよう設定
+    // ブラウザバックでもローディングアニメーションが表示されないよう設定
     window.addEventListener("pageshow", () => {
         this.hide();
       });
     }
   
+  // ユーザーが「data-action="click->loading#show"」属性のHTML要素をクリックしたら、以下が発動
+  // application.html.erbの「data-loading-target="spinner"」属性がある要素の「hidden」を取り除き、スピナーを表示
+  // HTMLの要素の中のclass~をjsから操作操作するための
   show() {
     this.spinnerTarget.classList.remove("hidden")
   }
   
+  // classListでhideenをHTML要素に追加し、スピナーを非表示にする
   hide() {
     this.spinnerTarget.classList.add("hidden")
   }
