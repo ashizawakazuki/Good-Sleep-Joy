dce0ec464739ab9c16af439062144fce3cc8d73e
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-06-09 00:07:09 +0900

    add:ユーザーに関するテストファイルを生成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    it 'nameとemailとpasswordとpassword_confirmationが有効であること' do
+      user = build(:user) 
+      expect(user).to be_valid
+    end
+
+    it 'nameが空だと無効であること' do
+      user = build(:user, name: nil)
+      user.valid? 
+      expect(user.errors[:name]).not_to be_empty
+    end
+
+    it 'emailが空だと無効であること' do
+      user = build(:user, email: nil)
+      user.valid? 
+      expect(user.errors[:email]).not_to be_empty
+    end
+
+    it 'passwordが空だと無効であること' do
+      user = build(:user, password: nil)
+      user.valid? 
+      expect(user.errors[:password]).not_to be_empty
+    end
+
+    it 'passwordが5文字以下だと無効であること' do
+      user = build(:user, password: "a" * 5)
+      user.valid?
+      expect(user.errors[:password]).not_to be_empty
+    end
+
+    it 'passwordが129文字以上だと無効であること' do
+      user = build(:user, password: "a" * 129)
+      user.valid?
+      expect(user.errors[:password]).not_to be_empty
+    end
+  end
 end
