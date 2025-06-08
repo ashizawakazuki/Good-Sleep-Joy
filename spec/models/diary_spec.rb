e5565d319209729bdf3e8a94c2c0758ca853c59f
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-06-08 19:02:53 +0900

    add:diary_spec_rbにテストコードを記述

--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Diary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    it 'dateとtitleとbodyとが有効であること' do
+      diary = build(:diary) 
+      expect(diary).to be_valid
+    end
+
+    it 'dateが空だと無効であること' do
+      diary = build(:diary, date: nil)
+      diary.valid?
+      expect(diary.errors[:date]).not_to be_empty
+    end
+
+    it 'titleが51文字以上だと無効であること' do
+      diary = build(:diary, title: "テストタイトル" * 8)
+      diary.valid?
+      expect(diary.errors[:title]).not_to be_empty
+    end
+
+    it 'titleが空だと無効であること' do
+      diary = build(:diary, title: nil)
+      diary.valid?
+      expect(diary.errors[:title]).not_to be_empty
+    end
+
+    it 'contentが1001文字以上だと無効であること' do
+      diary = build(:diary, content: "テスト本文" * 201)
+      diary.valid?
+      expect(diary.errors[:content]).not_to be_empty
+    end
+
+  end
 end
