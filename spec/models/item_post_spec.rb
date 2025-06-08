c23fe10117f80216bc111490242c061fafc95b0e
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-06-08 17:12:42 +0900

    add:item_post_specにバリデーションに関するテストコードを記述

--- spec/models/item_post_spec.rb
+++ spec/models/item_post_spec.rb
@@ -3,8 +3,32 @@ require 'rails_helper'
 RSpec.describe ItemPost, type: :model do #ItemPostのモデルであることを意味
   describe 'バリデーションチェック' do
     it 'titleとbodyが有効であること' do
-      item_post = build(:item_post)
+      item_post = build(:item_post) # factory botによりitem_post.rbよりデータを生成
       expect(item_post).to be_valid # be_validでitem_postがバリデーションが通るかを検証
+    end
+
+    it 'titleが空だと無効であること' do
+      item_post = build(:item_post, title: nil) # factory botにより生成されたtitleをnilに上書き
+      item_post.valid? # バリデーションを通るか確認
+      expect(item_post.errors[:title]).not_to be_empty # エラーメッセージが空でないかを判定
+    end
+
+    it 'titleが51文字以上だと無効であること' do
+      item_post = build(:item_post, title: "テストタイトル" * 8)
+      item_post.valid?
+      expect(item_post.errors[:title]).not_to be_empty
+    end
+
+    it 'bodyが空だと無効であること' do
+      item_post = build(:item_post, body: nil)
+      item_post.valid?
+      expect(item_post.errors[:body]).not_to be_empty
+    end
+
+    it 'bodyが1001文字以上だと無効であること' do
+      item_post = build(:item_post, body: "テスト本文" * 201)
+      item_post.valid?
+      expect(item_post.errors[:body]).not_to be_empty
     end
   end
 end
