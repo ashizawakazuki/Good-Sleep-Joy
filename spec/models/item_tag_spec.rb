cbe95e54f79db6d53a4f29939e2cfe81f6dc8d84
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-06-10 11:57:13 +0900

    feat:item_tag_spec.rbを記述

--- spec/models/item_tag_spec.rb
+++ spec/models/item_tag_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ItemTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    it 'nameが有効であること' do
+      item_tag = build(:item_tag)
+      expect(item_tag).to be_valid
+    end
+
+    it 'nameが空だと無効であること' do
+      item_tag = build(:item_tag, name: nil)
+      item_tag.valid?
+      expect(item_tag.errors[:name]).not_to be_empty
+    end
+  end
 end
