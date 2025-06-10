0564407020a4cddec06b49364adbffc7d46c3c67
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-06-10 11:52:56 +0900

    feat:habit_tag_spec.rbを記述

--- spec/models/habit_tag_spec.rb
+++ spec/models/habit_tag_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe HabitTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    it 'nameが有効であること' do
+      habit_tag = build(:habit_tag)
+      expect(habit_tag).to be_valid
+    end
+
+    it 'nameが空だと無効であること' do
+      habit_tag = build(:habit_tag, name: nil)
+      habit_tag.valid?
+      expect(habit_tag.errors[:name]).not_to be_empty
+    end
+  end
 end
